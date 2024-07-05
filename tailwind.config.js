04955166f0bcc44f8b7492c2b394a64cb405eac9
Naimor <rombelsecure@gmail.com>
2024-07-05 12:34:25 +0200

    feat: storybook

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,12 @@ import daisyui from "daisyui";
 
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./src/**/*.{js,ts,jsx,tsx,mdx}"],
+  content: [
+    "./src/**/*.{js,ts,jsx,tsx,mdx}",
+    'node_modules/daisyui/dist/**/*.js',
+    'node_modules/react-daisyui/dist/**/*.js',
+    
+  ],
   theme: {
     extend: {},
   },
